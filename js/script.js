5c473abe1fbafe6a95fdaef8c0873cc0f5393913
Daniel Svensson <daniel.svensson@purplescout.se>
2010-06-13 14:20:31 +0200

    Add Google Analytics to page.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-16943685-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
 // Flattr settings
 var flattr_btn = 'compact';
 var flattr_uid = '215';
@@ -24,6 +36,6 @@ getTwitters('tweet', {
 // Photo Showcase Cycle
 $('#showcase').cycle({
     fx: 'fade',
-	speed:    1500,
+    speed:    1500,
     timeout:  7000
 });
