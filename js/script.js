b42e39f8727e812a89dcaff6d724b8e43ff80be0
Daniel Svensson <daniel.svensson@purplescout.se>
2010-06-13 14:32:59 +0200

    Move Google Analytics to bottom of the page.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-// Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-16943685-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-
 // Flattr settings
 var flattr_btn = 'compact';
 var flattr_uid = '215';
